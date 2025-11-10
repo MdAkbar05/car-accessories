650d0eca98013f14e680fc9a3089195f8d8e73d7
MdAkbar05 <samratakbar667466@gmail.com>
2025-11-10 18:44:07 +0600

    change category

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,10 +1,10 @@
 import ProductFilter from "./FindProduct";
 
 export default async function Filters({ parentFilter }) {
-  const [categories, brands, models, engines] = await Promise.all([
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/categories`).then((r) =>
-      r.json()
-    ),
+  const categories = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/categories`
+  ).then((r) => r.json());
+  const [brands, models, engines] = await Promise.all([
     fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products/brands`).then((r) =>
       r.json()
     ),
